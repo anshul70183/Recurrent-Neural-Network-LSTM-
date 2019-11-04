aecd5faed542e0601638d37fb904ab0618c63b5f
anshul70183 <30386363+anshul70183@users.noreply.github.com>
2019-11-04 17:22:47 +0530

    Add files via upload

---
+++ run.sh
@@ -0,0 +1,2 @@
+python train.py --lr 0.001 --batch_size 128 --init 1 --save_dir 'D:DL\PA3_submission' --dropout_prob 0.02 --decode_method 1 --beam_width 0
+--epochs 5 --train train.csv --val valid.csv
